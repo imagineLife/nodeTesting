99eac754fab81bbff038a2760c076babd7d6ac67
Eric Laursen <ericjlaursen@gmail.com>
2020-01-30 06:55:38 -0500

    2 tests

---
+++ test/unit.js
@@ -0,0 +1,31 @@
+/*
+ * Unit Tests
+ *
+ */
+
+// Dependencies
+var tests = require('./../app/lib');
+var assert = require('assert');
+
+// Holder for Tests
+var unit = {};
+
+// Assert that the getANumber function is returning a number
+unit['multiplyBy3 should return a number with number input'] = function(done){
+  var val = tests.multiplyBy3(4);
+  let resType = typeof(val)
+  assert.equal(resType, 'number');
+  done();
+};
+
+unit['multiplyBy3 should return 12 with 4 input'] = function(done){
+  var val = tests.multiplyBy3(4);
+  assert.equal(val, 12);
+  done();
+};
+
+
+
+
+// Export the tests to the runner
+module.exports = unit;
