485c0087026cb87c4e9e05e0fc77dbcc7a3f061b
Eric Laursen <ericjlaursen@gmail.com>
2020-02-01 12:22:45 -0500

    updates return expectations

--- app/lib.js
+++ app/lib.js
@@ -4,10 +4,18 @@ const tests = {}
 
 tests.multiplyBy3 = function(param){
 	if(typeof param !== 'number'){
+		return {"Error": 'invalid param type'}
+	}else{
+		return param * 3
+	}
+}
+
+tests.passNumAndCB = function(param, cb){
+	if(typeof param !== 'number'){
 		var err = new Error('Incorrect parameter type');
   	throw(err);
 	}else{
-		return param * 3
+		return cb(param * 3)
 	}
 }
 
